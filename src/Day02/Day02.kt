481870d1d315fefc61174231b6577220f165089b
James King <jamesrking1995@gmail.com>
2022-12-05 14:59:45 +0000

    Complete Day02 Part 1.
    Turns out I was taking the options in backwards, using A, B and C as my options when actually they were the opponents choices. After switching these around the correct way, got the correct answer. *Facepalm*

--- src/Day02/Day02.kt
+++ src/Day02/Day02.kt
@@ -2,21 +2,10 @@ package Day02
 
 import readInput
 
-val lose = 0
-val draw = 3
-val won = 6
-
-val rock = 1 // A | X
-val paper = 2 // B | Y
-val scissors = 3 // C | Z
-
-
-val results = mapOf("A X" to 4, "A Y" to 1, "A Z" to 7, "B X" to 8, "B Y" to 5, "B Z" to 2, "C X" to 3, "C Y" to 9, "C Z" to 6)
-
 fun main() {
 
     val testInput = readInput("Day02/Day02_test")
-//    check(part1(testInput) == 15)
+    check(part1(testInput) == 15)
 
     val input = readInput("Day02/Day02")
 
@@ -26,21 +15,16 @@ fun main() {
 
 fun part1(input: List<String>): Int {
     var totalScore = 0
-        input.forEach(){ round ->
-        totalScore += playNewMatch(round)
-//        val roundPicks = round.split(" ")
-//        playMatch(roundPicks[0], roundPicks[1])
+        input.forEach{ round ->
+        val roundPicks = round.split(" ")
+        totalScore += playMatch(roundPicks[0], roundPicks[1])
     }
     return totalScore
 }
 
-fun playNewMatch(round: String): Int {
-    return results[round]!!
-}
-
-fun playMatch(myPick: String, opponentPick: String): Int {
-    val myPickOption = getOptionFromCode(myPick)
+fun playMatch(opponentPick: String, myPick: String): Int {
     val opponentPickOption = getOptionFromCode(opponentPick)
+    val myPickOption = getOptionFromCode(myPick)
     val outcomeScore = if((myPickOption == Option.ROCK && opponentPickOption == Option.SCISSORS) || (myPickOption.weight > opponentPickOption.weight && !(myPickOption == Option.SCISSORS && opponentPickOption == Option.ROCK))){
         6
     } else if(myPickOption.weight == opponentPickOption.weight) {
