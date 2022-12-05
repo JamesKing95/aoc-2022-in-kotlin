3810b40959f802637099b9065696668356d3170e
James King <jamesrking1995@gmail.com>
2022-12-05 15:20:09 +0000

    Set up Day03 for later one when I come to it.

---
+++ src/Day03/Day03.kt
@@ -0,0 +1,23 @@
+package Day03
+
+import readInput
+
+fun main() {
+
+    val testInput = readInput("Day03/Day03_test")
+    check(part1(testInput) == 15)
+    check(part2(testInput) == 12)
+
+    val input = readInput("Day03/Day03")
+
+    println("Part 1: ".plus(part1(input)))
+    println("Part 2: ".plus(part2(input)))
+}
+
+fun part1(input: List<String>): Int {
+    return 0
+}
+
+fun part2(input: List<String>): Int {
+    return 0
+}
