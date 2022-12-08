6e0832e59db5c2b5dd1fecc99b77090229cd3b8c
James King <jamesrking1995@gmail.com>
2022-12-08 20:24:52 +0000

    Solve Day03 part 1.

--- src/Day03/Day03.kt
+++ src/Day03/Day03.kt
@@ -2,11 +2,13 @@ package Day03
 
 import readInput
 
+private val ALPHABET_INDEX = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 fun main() {
 
     val testInput = readInput("Day03/Day03_test")
-    check(part1(testInput) == 15)
-    check(part2(testInput) == 12)
+    check(part1(testInput) == 157)
+//    check(part2(testInput) == 12)
 
     val input = readInput("Day03/Day03")
 
@@ -15,7 +17,24 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    return 0
+    var total = 0
+    input.forEach{
+        // Split the line in half
+        val halfWayPoint = it.length / 2
+        val firstHalf = it.take(halfWayPoint)
+        val secondHalf = it.takeLast(halfWayPoint).split("").drop(1).dropLast(1)
+
+        // Compare two halves and find common value
+        val matchingCharacter = firstHalf.findAnyOf(secondHalf, 0, false)!!.second
+
+        // Compare common value to priority table.
+        val priority = ALPHABET_INDEX.indexOf(matchingCharacter) + 1
+
+        // Add up all priority values.
+        total += priority
+    }
+
+    return total
 }
 
 fun part2(input: List<String>): Int {
