cf7b6bd5ae6e13b83d871dfd6f0a75b6ae8f04cf
James King <jamesrking1995@gmail.com>
2022-12-08 21:13:34 +0000

    Solve Day03 part 2.

--- src/Day03/Day03.kt
+++ src/Day03/Day03.kt
@@ -8,7 +8,7 @@ fun main() {
 
     val testInput = readInput("Day03/Day03_test")
     check(part1(testInput) == 157)
-//    check(part2(testInput) == 12)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03/Day03")
 
@@ -38,5 +38,40 @@ fun part1(input: List<String>): Int {
 }
 
 fun part2(input: List<String>): Int {
-    return 0
+    //Find groups of 3 lines
+    val currentGroup = mutableListOf<String>()
+    var total = 0
+    input.forEach{
+        currentGroup.add(it)
+        if(currentGroup.size == 3) {
+            val firstElf = currentGroup[0]
+            val secondElf = currentGroup[1].split("").drop(1).dropLast(1).toMutableList()
+
+            // Find all common items between Elf one and two
+            val elfOneAndTwoCommonItems = mutableListOf<String>()
+
+            var currentMatchingItem = firstElf.findAnyOf(secondElf, 0, false)?.second
+            while(!currentMatchingItem.isNullOrBlank()) {
+                elfOneAndTwoCommonItems.add(currentMatchingItem)
+                while(secondElf.contains(currentMatchingItem)) { // Remove any duplicate matching items from secondElf to not match them again.
+                    secondElf.remove(currentMatchingItem)
+                }
+                currentMatchingItem = firstElf.findAnyOf(secondElf, 0, false)?.second
+            }
+
+            // Find the one matching item between elf three and array of all matches between one and two.
+            val idCardValue = currentGroup[2].findAnyOf(elfOneAndTwoCommonItems, 0, false)!!.second
+
+            // Calculate priority value from index
+            val priority = ALPHABET_INDEX.indexOf(idCardValue) + 1
+
+            // Add to total
+            total += priority
+
+            // Clear current working group so can focus on the next three elves.
+            currentGroup.clear()
+        }
+    }
+
+    return total
 }
